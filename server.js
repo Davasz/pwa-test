aac5441f8ca3966054108f41b39500add858401f
Davasz <davidavi200615@gmail.com>
2023-12-29 18:17:01 -0300

    notificações indviduais

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const PushNotifications = require("@pusher/push-notifications-server");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+const beamsClient = new PushNotifications({
+  instanceId: "eeb902ab-af5a-482c-ad98-77adc283449c",
+  secretKey: "B3FCAABB8C4C1E88572D266E800A76B77B61B12C02C6930142B70214CA60A538",
+});
+
+app.get("/auth", function (req, res) {
+  const userId = req.query["user_id"];
+  const userIDInQueryParam = req.query["user_id"];
+  console.log(userId)
+  if (userId != userIDInQueryParam) {
+    res.status(401).send("Inconsistent request");
+  } else {
+    const beamsToken = beamsClient.generateToken(userId);
+    res.send(JSON.stringify(beamsToken));
+  }
+});
+
+app.get("/not/:id", (req, res) => {
+  const id = req.params.id
+  beamsClient
+  .publishToUsers([id], {
+    apns: {
+      aps: {
+        alert: {
+          title: "Hello",
+          body: "Hello, world!",
+        },
+      },
+    },
+    fcm: {
+      notification: {
+        title: "Hello",
+        body: "Hello, world!",
+      },
+    },
+    web: {
+      notification: {
+        title: "Hello",
+        body: "Hello, world!",
+      },
+    },
+  })
+  .then((publishResponse) => {
+
+    req.res.send(publishResponse.publishId)
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor Express rodando na porta ${PORT}`);
